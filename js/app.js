58268c253cb45682e9b674e7a24f884a5b7c11f6
Jon Cramer <joncramer96@gmail.com>
2023-03-15 06:01:03 -0400

    set global variables and event listener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+/*===============================================================
+                    GLOBAL VARIABLES
+===============================================================*/
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
+const resetButton = document.querySelector('a');
+const startScreen = document.querySelector('.start');
+const gameScreen = document.querySelector('.section');
+
+/*===============================================================
+                        EVENTS
+===============================================================*/
+resetButton.addEventListener('click', () => {
+    startScreen.style.display = 'none';
+});
